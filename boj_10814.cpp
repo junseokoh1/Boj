a46cffca6b096350035ecd957ffdaebb91cd15e4
JunseokOh <21500412@handong.edu>
2021-04-11 20:36:02 +0900

    Create boj_10814.cpp

---
+++ boj_10814.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+
+//a가 b의 앞에 와야할 때 true
+//그렇지 않을 때에는 false
+bool cmp(pair<int, string> a, pair<int, string> b){
+
+    //나이가 같은 경우
+    //if(a.first == b.first)  
+
+    return a.first < b.first;
+};
+
+int main() {
+
+  vector<pair<int, string>> vec;
+  int N, age;
+  string name;
+
+  cin>>N;
+
+  for(int i=0; i< N; i++){
+    cin>>age>>name;
+    vec.push_back(make_pair(age, name));
+  }
+  
+  //pair를 sort할 때 cmp함수 설정 없이 그냥 해주면 first를 기준으로 정렬을 해줌!
+  //이름은 먼저 가입 기준임으로 stable sort
+   stable_sort(vec.begin(), vec.end(), cmp);
+  //sort(vec.begin(), vec.end(), cmp);        이걸로하면 틀림!
+
+  //vector iteration
+  vector<pair<int, string>>::iterator iter;
+  for(iter = vec.begin(); iter != vec.end(); iter++)
+      cout<<(*iter).first<<" "<<iter->second<<'\n';
+
+}
